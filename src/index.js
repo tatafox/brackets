32622e0378d258c24dde74f6f1a2be3551e0d4eb
Ваше имя@ <you@example.com>
2020-10-07 09:50:19 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+ 
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            let index = str.indexOf(bracketsConfig[j].join(''));
+            if (index !== -1) {
+                str = str.replace(bracketsConfig[j].join(''), '');
+                i = -1;
+            }
+        }
+    }
+
+    return (str.length === 0);
+    
+}    
+
+
+  
